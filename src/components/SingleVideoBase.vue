68630e5583c531e1aaa8a14d826ada1b610fb2b6
hiba <hiba.mashfij@gmail.com>
2021-06-03 11:03:16 +0300

    created VideoBase and VideoTitle components

---
+++ src/components/SingleVideoBase.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
+    <div class="h-60">
+      <iframe
+        :src="`https://www.loom.com/embed/${link}`"
+        frameborder="0"
+        webkitallowfullscreen
+        mozallowfullscreen
+        allowfullscreen
+        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoBase",
+  props: ["link"],
+  setup() {},
+};
+</script>
