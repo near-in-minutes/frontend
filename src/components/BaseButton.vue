9a50e7436c2ee839ff6943c8146aab710cc53e2c
hiba <hiba.mashfij@gmail.com>
2021-06-16 19:10:07 +0300

    Update BaseButton.vue

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -2,7 +2,7 @@
   <div
     class="max-w-2xl mx-auto text-center pb-28 px-4 sm:py-20 sm:px-6 lg:px-8"
   >
-    <router-link to="/videos">
+    <router-link :to="link">
       <div
         class="
           mt-8
@@ -31,7 +31,7 @@
 
 <script>
 export default {
-  name: "VideosButton",
-  props: ["btnText"],
+  name: "BaseButton",
+  props: ["btnText", "link"],
 };
 </script>
