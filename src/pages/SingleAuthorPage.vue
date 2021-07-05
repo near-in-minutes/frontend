1d231c9c257081ad10d255991e23517cb6b733d2
cagatay soyer <cagataysoyer@gmail.com>
2021-07-05 15:04:42 +0300

    installed moment js, router link added for each author

--- src/pages/SingleAuthorPage.vue
+++ src/pages/SingleAuthorPage.vue
@@ -1,15 +1,16 @@
 <template>
-  <h1>{{ t('navbar.authors') }}</h1>
+  <h1>{{ t("navbar.authors") }}</h1>
 </template>
 
 <script>
-import { useI18n } from 'vue-i18n';
+import { useI18n } from "vue-i18n";
+import moment from "moment";
 
 export default {
   setup() {
-    const { t } = useI18n({ useScope: 'global' });
+    const { t } = useI18n({ useScope: "global" });
 
-    return { t };
-  }
+    return { t, moment };
+  },
 };
 </script>
