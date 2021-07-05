27f9a45d6fa49e3b72b8c5bc16e80391d95ec9c1
bigdaddy-zephyr <anujduggal61@gmail.com>
2021-07-05 14:13:21 +0530

    added ShareButton

--- src/components/SingleVideo.vue
+++ src/components/SingleVideo.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
     <div class="h-60">
-      <iframe :src="`https://www.loom.com/embed/${id}`" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%" />
+      <iframe
+        :src="`https://www.loom.com/embed/${id}`"
+        frameborder="0"
+        webkitallowfullscreen
+        mozallowfullscreen
+        allowfullscreen
+        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"
+      />
     </div>
   </div>
+  <share-button :path="path"> </share-button>
 </template>
 
 <script>
+import ShareButton from "@/components/ShareButton.vue";
+import { ref } from "vue";
 export default {
+  setup() {
+    const path = ref("https://near-in-minutes.com" + window.location.pathname);
+    return { path };
+  },
   props: {
     id: {
       type: String,
-      required: true
-    }
-  }
+      required: true,
+    },
+  },
+  components: {
+    ShareButton,
+  },
 };
 </script>
