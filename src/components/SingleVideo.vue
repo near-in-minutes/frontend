d783ca700beb80f4692fff172f77718c82a459bf
bigdaddy-zephyr <anujduggal61@gmail.com>
2021-07-06 06:46:49 +0530

    formatting changes

--- src/components/SingleVideo.vue
+++ src/components/SingleVideo.vue
@@ -1,17 +1,10 @@
 <template>
   <div class="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
     <div class="h-60">
-      <iframe
-        :src="`https://www.loom.com/embed/${id}`"
-        frameborder="0"
-        webkitallowfullscreen
-        mozallowfullscreen
-        allowfullscreen
-        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"
-      />
+      <iframe :src="`https://www.loom.com/embed/${id}`" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%" />
     </div>
   </div>
-  <share-button :path="path"> </share-button>
+  <ShareButton :path="path"> </ShareButton>
 </template>
 
 <script>
