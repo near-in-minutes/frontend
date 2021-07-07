f1a092b155309998313ae9435460076899e73d13
Sherif Abushadi <sherif@amgando.org>
2021-07-07 11:10:07 +0300

    formatting

--- src/components/AuthorsList.vue
+++ src/components/AuthorsList.vue
@@ -1,71 +1,20 @@
 <template>
-  <ul
-    class="
-      pt-12
-      grid grid-cols-1
-      gap-6
-      sm:grid-cols-2
-      md:grid-cols-3
-      lg:grid-cols-4
-    "
-  >
-    <li
-      v-for="author in authors"
-      :key="author.githubUname"
-      class="col-span-1 flex flex-col text-center bg-white rounded-lg"
-    >
+  <ul class="pt-12 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <li v-for="author in authors" :key="author.githubUname" class="col-span-1 flex flex-col text-center bg-white rounded-lg">
       <router-link :to="{ name: 'author', params: { id: author.id } }">
-        <div
-          class="
-            group
-            block
-            w-full
-            py-3
-            aspect-w-10 aspect-h-7
-            rounded-lg
-            bg-gray-50
-          "
-        >
-          <img
-            class="
-              w-32
-              h-32
-              flex-shrink-0
-              mx-auto
-              bg-black
-              rounded-full
-              object-cover
-              pointer-events-none
-              group-hover:opacity-75
-            "
-            :src="author.githubAvatar"
-            alt="profileImg"
-          />
+        <div class="group block w-full py-3 aspect-w-10 aspect-h-7 rounded-lg bg-gray-50">
+          <img class="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full object-cover pointer-events-none group-hover:opacity-75" :src="author.githubAvatar" alt="profileImg" />
           <h3 class="mt-6 text-gray-900 text-sm font-medium">
             {{ author.name }}
           </h3>
           <p class="text-sm mt-3 text-near-royal">
-            <a :href="author.github" target="_new">
-              github / {{ author.githubUname }}
-            </a>
+            <a :href="author.github" target="_new"> github / {{ author.githubUname }} </a>
           </p>
 
-          <div
-            class="
-              mt-6
-              py-5
-              sm:p-6
-              border-t border-gray-300
-              text-left
-              mx-12
-              space-y-1
-            "
-          >
+          <div class="mt-6 py-5 sm:p-6 border-t border-gray-300 text-left mx-12 space-y-1">
             <p class="text-sm flex justify-between">
               <span class="text-gray-600">joined: </span>
-              <span class="text-near-green">{{
-                formatDate(author.joined)
-              }}</span>
+              <span class="text-near-green">{{ formatDate(author.joined) }}</span>
             </p>
             <p v-if="author.contributions" class="text-sm flex justify-between">
               <span class="text-gray-600">videos:</span>
@@ -74,22 +23,7 @@
             <p class="text-sm flex justify-between text-right">
               <span class="text-gray-600">languages:</span>
               <span>
-                <span
-                  v-for="lang in author.languages"
-                  :key="lang"
-                  class="
-                    inline-flex
-                    px-2
-                    py-0.5
-                    ml-2
-                    my-1
-                    rounded
-                    text-xs
-                    font-medium
-                    bg-gray-200
-                    text-near-green
-                  "
-                >
+                <span v-for="lang in author.languages" :key="lang" class="inline-flex px-2 py-0.5 ml-2 my-1 rounded text-xs font-medium bg-gray-200 text-near-green">
                   {{ lang }}
                 </span>
               </span>
@@ -101,15 +35,15 @@
   </ul>
 </template>
 <script>
-import { onMounted } from "vue";
+import { onMounted } from 'vue';
 
-import { useI18n } from "vue-i18n";
+import { useI18n } from 'vue-i18n';
 
-import { useAuthors } from "@/composables/useAuthors";
+import { useAuthors } from '@/composables/useAuthors';
 
 export default {
   setup() {
-    const { t } = useI18n({ useScope: "global" });
+    const { t } = useI18n({ useScope: 'global' });
     const { authors, fetchAllAuthors } = useAuthors();
     // console.log(authors);
 
@@ -117,14 +51,14 @@ export default {
 
     return {
       t,
-      authors,
+      authors
     };
   },
   methods: {
     formatDate(d) {
       const date = new Date(d);
       return date.toLocaleDateString();
-    },
-  },
+    }
+  }
 };
 </script>
--- src/components/VideoListByAuthor.vue
+++ src/components/VideoListByAuthor.vue
@@ -2,45 +2,17 @@
 <template>
   <!-- <h1>{{ videoDetails }}</h1> -->
   <div class="w-full flex justify-center mt-8">
-    <ul
-      class="
-        max-w-7xl
-        grid grid-cols-1
-        gap-y-6 gap-x-12
-        sm:grid-cols-2
-        lg:grid-cols-3
-      "
-    >
-      <li
-        v-for="video in videoDetails"
-        :key="video.content"
-        class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200"
-      >
+    <ul class="max-w-7xl grid grid-cols-1 gap-y-6 gap-x-12 sm:grid-cols-2 lg:grid-cols-3">
+      <li v-for="video in videoDetails" :key="video.content" class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
         <!-- Goes to Loom website for now -->
-        <router-link
-          :to="{ name: 'video', params: { id: video.fields.content[0] } }"
-        >
+        <router-link :to="{ name: 'video', params: { id: video.fields.content[0] } }">
           <div class="w-full flex items-center justify-between p-6 space-x-6">
             <div class="flex-1 truncate">
               <div class="flex items-center space-x-3">
                 <h3 class="text-gray-900 text-sm font-medium truncate">
                   {{ video.fields.title }}
                 </h3>
-                <span
-                  class="
-                    flex-shrink-0
-                    inline-block
-                    px-2
-                    py-0.5
-                    text-green-800 text-xs
-                    font-medium
-                    bg-green-100
-                    rounded-full
-                  "
-                  >{{
-                    video.fields.content_tags.toString().replaceAll(",", " ")
-                  }}</span
-                >
+                <span class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">{{ video.fields.content_tags.toString().replaceAll(',', ' ') }}</span>
               </div>
               <p class="mt-1 text-gray-500 text-sm truncate">
                 {{ video.fields.subheadline }}
@@ -61,15 +33,15 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref } from 'vue';
 
-import moment from "moment";
+import moment from 'moment';
 
 // import { useAuthors } from "@/composables/useAuthors";
-import { findOneContent } from "../services/airtable/index";
+import { findOneContent } from '../services/airtable/index';
 
 export default {
-  props: ["content", "author"],
+  props: ['content', 'author'],
   async setup(props) {
     const videoDetails = ref([]);
 
@@ -80,8 +52,8 @@ export default {
     // console.log(videoContentId);
     // console.log(authorId);
 
-    await videoContentId.forEach((vc) => {
-      findOneContent(vc).then((res) => {
+    await videoContentId.forEach(vc => {
+      findOneContent(vc).then(res => {
         // console.log(res.fields);
         return videoDetails.value.push(res);
       });
@@ -95,8 +67,8 @@ export default {
     return {
       videoDetails,
       authorId,
-      formatDate,
+      formatDate
     };
-  },
+  }
 };
 </script>
