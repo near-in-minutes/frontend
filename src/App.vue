9f725964d428f5364485de18281319ff6efb908a
oceanByte <albert.peci@posteo.de>
2021-07-16 12:02:21 +0200

    Revert "feat(global): add meta tags"
    This reverts commit 35a503844b804838ca9c85a9a4536ccc23bbcf35.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,4 @@
 <template>
-  <teleport to="head">
-    <meta property="title" :content="metadata.title">
-    <meta property="description" :content="metadata.description">
-    <meta property="og:title" :content="metadata.title">
-    <meta property="og:description" :content="metadata.description">
-    <meta property="og:image" content="https://near-in-minutes.com/img/logo.dbccf5a9.png" />
-    <meta property="og:type" content="website" />
-    <meta property="og:site_name" content="NEAR in minutes" />
-    <meta property="twitter:card" content="summary">
-    <meta property="twitter:site" content="@NEARProtocol">
-  </teleport>
   <NavigationComponent />
   <Suspense>
     <router-view />
@@ -18,9 +7,6 @@
 </template>
 
 <script>
-import { computed, reactive } from 'vue';
-import { useI18n } from 'vue-i18n';
-
 import NavigationComponent from "@/components/layout/NavigationComponent";
 import FooterComponent from "@/components/layout/FooterComponent";
 
@@ -29,17 +15,5 @@ export default {
     NavigationComponent,
     FooterComponent,
   },
-  setup() {
-    const { t } = useI18n({ useScope: 'global' });
-
-    const metadata = reactive(
-      {
-        title: computed(() => t('metadata.title')),
-        description: computed(() => t('metadata.description'))
-      }
-    );
-
-    return { t, metadata };
-  }
 };
 </script>
