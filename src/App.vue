f111a5d769a12e77ac04f3f4e7ed85a260074d0a
oceanByte <albert.peci@posteo.de>
2021-08-04 13:55:03 +0200

    refactor: run yarn fix

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <teleport to="head">
-    <meta property="title" :content="metadata.title">
-    <meta property="description" :content="metadata.description">
-    <meta property="og:title" :content="metadata.title">
-    <meta property="og:description" :content="metadata.description">
+    <meta property="title" :content="metadata.title" />
+    <meta property="description" :content="metadata.description" />
+    <meta property="og:title" :content="metadata.title" />
+    <meta property="og:description" :content="metadata.description" />
     <meta property="og:image" content="https://near-in-minutes.com/og-preview-image.jpg" />
     <meta property="og:type" content="website" />
     <meta property="og:site_name" content="NEAR in minutes" />
-    <meta property="twitter:card" content="summary">
-    <meta property="twitter:site" content="@NEARProtocol">
+    <meta property="twitter:card" content="summary" />
+    <meta property="twitter:site" content="@NEARProtocol" />
   </teleport>
   <NavigationComponent />
   <Suspense>
@@ -21,24 +21,21 @@
 import { computed, reactive } from 'vue';
 import { useI18n } from 'vue-i18n';
 
-import NavigationComponent from "@/components/layout/NavigationComponent";
-import FooterComponent from "@/components/layout/FooterComponent";
-
+import NavigationComponent from '@/components/layout/NavigationComponent';
+import FooterComponent from '@/components/layout/FooterComponent';
 
 export default {
   components: {
     NavigationComponent,
-    FooterComponent,
+    FooterComponent
   },
   setup() {
     const { t } = useI18n({ useScope: 'global' });
 
-    const metadata = reactive(
-      {
-        title: computed(() => t('metadata.title')),
-        description: computed(() => t('metadata.description'))
-      }
-    );
+    const metadata = reactive({
+      title: computed(() => t('metadata.title')),
+      description: computed(() => t('metadata.description'))
+    });
 
     return { t, metadata };
   }
