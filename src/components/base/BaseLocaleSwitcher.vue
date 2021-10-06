687f24ddc1a42ce8db5b5813c8a341de59921d9f
hiba <hiba.mashfij@gmail.com>
2021-10-06 22:18:34 +0300

    update navbar

--- src/components/base/BaseLocaleSwitcher.vue
+++ src/components/base/BaseLocaleSwitcher.vue
@@ -1,6 +1,6 @@
 <template>
   <Listbox as="div" v-model="selected">
-    <div class="relative w-44">
+    <div class="relative w-24">
       <ListboxButton class="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
         <div class="flex items-center">
           <span :aria-label="selected.available ? 'available' : 'unavailable'" :class="[selected.available ? 'bg-green-400' : 'bg-gray-200', 'flex-shrink-0 inline-block h-2 w-2 rounded-full']" />
@@ -12,7 +12,7 @@
       </ListboxButton>
 
       <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
-        <ListboxOptions class="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+        <ListboxOptions class="absolute z-10 mt-1 w-28 bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
           <ListboxOption as="template" v-for="(lang, index) in languages" :key="index" :value="lang" v-slot="{ active, selected }">
             <li :class="[active ? 'text-white bg-indigo-600' : 'text-gray-900', 'cursor-default select-none relative py-2 pl-3 pr-9']">
               <div class="flex items-center">
@@ -41,13 +41,13 @@ import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headless
 import { CheckIcon, SelectorIcon } from '@heroicons/vue/solid';
 
 const languages = [
-  { name: 'English', code: 'en', available: true },
-  { name: 'Chinese', code: 'zh', available: false },
-  { name: 'Hindi', code: 'hi', available: true },
-  { name: 'Russian', code: 'ru', available: false },
-  { name: 'Spanish', code: 'es', available: true },
-  { name: 'Turkish', code: 'tr', available: true },
-  { name: 'German', code: 'de', available: true }
+  { name: 'En', code: 'en', available: true },
+  { name: 'Zh', code: 'zh', available: false },
+  { name: 'Hi', code: 'hi', available: true },
+  { name: 'Ru', code: 'ru', available: false },
+  { name: 'Es', code: 'es', available: true },
+  { name: 'Tr', code: 'tr', available: true },
+  { name: 'De', code: 'de', available: true }
 ];
 
 export default {
