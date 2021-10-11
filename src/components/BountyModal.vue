68b48f496164e39727ed13b8644e8858fb120c44
hiba <hiba.mashfij@gmail.com>
2021-10-11 17:24:42 +0300

    Update BountyModal.vue

--- src/components/BountyModal.vue
+++ src/components/BountyModal.vue
@@ -1,44 +1,38 @@
 <template>
-  
-    <Dialog as="template" class="fixed z-10 inset-0 overflow-y-auto" :open="isOpen" @close="setBountyDoneModal">
-      <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-      
-          <DialogOverlay class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-     
-        <!-- This element is to trick the browser into centering the modal contents. -->
-        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-       
-          <div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-            <div>
-              <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100"></div>
-              <div class="mt-3 text-center sm:mt-5">
-                <DialogTitle as="h3" class="text-lg leading-6 font-medium text-gray-900"> Payment successful </DialogTitle>
-                <div class="mt-2">
-                  <p>proposer:{{bountyDone.data.proposer}}</p>
-                           <p>description:{{bountyDone.data.description}}</p>
-                                    <p>receiver_id:{{bountyDone.data.receiver_id}}</p>
-                                            <p>status:{{bountyDone.status}}</p>
-                                              <p>submission_time:{{bountyDone.submission_time}}</p>
-                  <p class="text-sm text-gray-500">
-                   
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
-              <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm" @click="setBountyDoneModal(false, bountyDone)">Deactivate</button>
-              <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm" @click="setBountyDoneModal(false, bountyDone)" ref="cancelButtonRef">Cancel</button>
-            </div>
-          </div>
-     
-      </div>
-    </Dialog>
+  <Dialog as="template" class="fixed z-10 inset-0 overflow-y-auto" :open="isOpen" @close="setBountyDoneModal">
+    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <DialogOverlay class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
 
+      <!-- This element is to trick the browser into centering the modal contents. -->
+      <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+      <div class="inline-block align-bottom bg-white rounded-2xl text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+        <div class="">
+          <DialogTitle as="h3" class="text-lg font-medium text-gray-900 bg-red-200 pl-10 pr-40 py-20">
+            <p class="">{{ bountyDone.description }}</p>
+            <p class="text-sm text-gray-700">Proposed by: {{ bountyDone.proposer }}</p>
+          </DialogTitle>
+
+          <div class="flex justify-between px-10 pt-10">
+            <p>
+              Bounty reciever: <br />
+              <span class="font-semibold">{{ bountyDone.kind.BountyDone.receiver_id }}</span>
+            </p>
+            <p v-if="bountyDone.status === 'Approved'" class="px-6 py-3 inline-flex text-xs leading-5 font-semibold rounded-full border border-near-red">{{ bountyDone.status }}</p>
+          </div>
+
+          <p class="text-sm text-gray-500 px-10 pt-2">{{ bountyDone.submission_time }}</p>
+        </div>
+        <div class="mt-5 flex justify-center p-5">
+          <button type="button" class="w-48 inline-flex justify-center rounded-lg border border-transparent shadow-sm px-4 py-2 bg-near-green text-xl font-medium text-white hover:bg-green-600 sm:text-sm" @click="setBountyDoneModal(false, bountyDone)">Ok</button>
+        </div>
+      </div>
+    </div>
+  </Dialog>
 </template>
 
 <script>
-import { Dialog, DialogOverlay, DialogTitle, } from '@headlessui/vue';
-
+import { Dialog, DialogOverlay, DialogTitle } from '@headlessui/vue';
 
 export default {
   components: { Dialog, DialogOverlay, DialogTitle },
@@ -57,10 +51,7 @@ export default {
     }
   },
   setup() {
-
-    return {
-
-    };
+    return {};
   }
 };
 </script>
