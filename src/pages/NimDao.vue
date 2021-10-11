906ea7a145b3482659f0062b381c2aca00617baf
hiba <hiba.mashfij@gmail.com>
2021-10-11 17:25:06 +0300

    Update NimDao.vue

--- src/pages/NimDao.vue
+++ src/pages/NimDao.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="relative pt-20 px-4 sm:px-6 lg:pb-28 lg:px-8">
-    <div class="background relative max-w-6xl m-auto rounded-3xl h-96">
-      
-    </div>
+  <div class="relative pt-10  sm:px-6 lg:pb-28 ">
+    <DaoHeroSection />
+    <DaoInfo/>
 
-    <div class="mt-20 max-w-6xl mx-auto space-y-6">
-      <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2">
-        <li v-for="bounty in bounties" :key="bounty.info.id" class="bg-white shadow overflow-hidden py-4 sm:rounded-md">
+    <div class="mt-20 pb-20 bg-near-gray">
+       <div class="pt-10 my-20 text-center">
+        <h2 class="text-3xl tracking-tight font-extrabold sm:text-4xl text-near-green">
+          Bounties
+        </h2>
+         <p class="mt-3 text-xl sm:mt-4 text-near-gray-dark">
+          Explore all the availble bounties
+        </p>
+      </div>
+
+      <ul role="list" class="max-w-6xl  mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2">
+        <li v-for="bounty in bounties" :key="bounty.info.id" class="bg-white shadow overflow-hidden py-4 sm:rounded-2xl">
           <div class="flex justify-between my-6 text-gray-500 sm:px-6 px-4">
             <p class="truncate text-xl text-near-green">
               {{ bounty.amount }}
@@ -27,17 +35,18 @@
           </div>
           <div class="mt-6 pt-4 flex border-t border-gray-200">
             <div v-if="bounty.info.times > bounty.claimNum" class="w-0 flex-1 flex">
-              <button @click="handleClaimBounty(bounty.info.id, bounty.info.max_deadline)" class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">Claim</button>
+              <button @click="setBountyClaimModal(true, bounty.info.id, bounty.info.max_deadline)" class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-near-green">Claim</button>
             </div>
             <div v-else-if="bounty.bountyDone" class="w-0 flex-1 flex">
-              <button @click="setBountyDoneModal(true, bounty.bountyDone)" class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg">View bounty done proposal</button>
+              <button @click="setBountyDoneModal(true, bounty.bountyDone)" class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-near-green">View bounty done proposal</button>
             </div>
             <div v-else class="w-0 flex-1 flex">
               <p class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg">Already claimed</p>
             </div>
           </div>
         </li>
-        <BountyModal :isOpen="isOpen" :setBountyDoneModal="setBountyDoneModal" :bountyDone="bountyDone" />
+        <BountyModal :isOpen="isOpen" :setBountyDoneModal="setBountyDoneModal" :bountyDone="bountyDone.data" />
+        <ClaimBountyModal :isOpen="isClaimOpen" :setBountyClaimModal="setBountyClaimModal" :id="claimId" :claimDeadline="claimDeadline" :handleClaimBounty="handleClaimBounty" />
       </ul>
     </div>
   </div>
@@ -50,22 +59,39 @@ import { format, fromUnixTime } from 'date-fns';
 import { CalendarIcon } from '@heroicons/vue/solid';
 import { ref, reactive } from 'vue';
 import BountyModal from '@/components/BountyModal';
+import ClaimBountyModal from '@/components/ClaimBountyModal';
+import DaoHeroSection from '@/components/DaoHeroSection';
+import DaoInfo from '@/components/DaoInfo'
 
 export default {
   components: {
     CalendarIcon,
-    BountyModal
+    BountyModal,
+    ClaimBountyModal,
+    DaoHeroSection,
+    DaoInfo
   },
   setup() {
     const { t } = useI18n({ useScope: 'global' });
     let isOpen = ref(false);
-    let bountyDone = reactive({ data: {} });
 
-    const { bounties, handleClaimBounty } = useNear();
+    let bountyDone = reactive({ data: {} });
 
     const setBountyDoneModal = (value, BountyDone) => {
       isOpen.value = value;
       bountyDone.data = BountyDone;
+    };
+
+    const { bounties, handleClaimBounty } = useNear();
+
+    let isClaimOpen = ref(false);
+    let claimId = ref();
+    let claimDeadline = ref()
+
+    const setBountyClaimModal = (value, id, deadline) => {
+      isClaimOpen.value = value;
+      claimId.value = id;
+      claimDeadline.value = deadline
     };
 
     return {
@@ -76,41 +102,12 @@ export default {
       bounties,
       bountyDone,
       isOpen,
-      setBountyDoneModal
+      setBountyDoneModal,
+      isClaimOpen,
+      setBountyClaimModal,
+      claimId,
+      claimDeadline
     };
   }
 };
 </script>
-
-
-<style lang="postcss" scoped>
-.background {
-  animation: colorchange 5s ease-in-out infinite ;
-  -webkit-animation: colorchange ease-in-out 5s infinite ;
-}
-
-@keyframes colorchange {
-  0% {
-    @apply bg-near-red-light;
-  }
-
-  50% {
-    @apply bg-near-gray;
-  }
-  100% {
-    @apply bg-near-red-light;
-  }
-}
-@-webkit-keyframes colorchange /* Safari and Chrome - necessary duplicate */ {
-  0% {
-    @apply bg-near-red-light;
-  }
-
-  50% {
-    @apply bg-near-gray;
-  }
-  100% {
-    @apply bg-near-red-light;
-  }
-}
-</style>
