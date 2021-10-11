712698fc82a4bc0ae31a594a34bfadadff9520d0
hiba <hiba.mashfij@gmail.com>
2021-10-11 17:25:02 +0300

    Update useBounties.js

--- src/composables/useBounties.js
+++ src/composables/useBounties.js
@@ -1,57 +1,32 @@
-import {
-  ref,
-  onMounted
-} from "vue";
-import {
-  getBountyClaims,
-  getBounty,
-  BountyDone,
-  BountyGiveUp,
-} from "../services/near";
-import {
-  fromUnixTime,
-  format,
-} from "date-fns";
+import { ref, onMounted } from 'vue';
+import { getBountyClaims, getBounty, BountyDone, BountyGiveUp } from '../services/near';
+import { fromUnixTime, format } from 'date-fns';
 
-import {
-  NEAR
-} from "near-units";
+import { NEAR } from 'near-units';
 
-export const useBounties = (accountId) => {
+export const useBounties = accountId => {
   const userBounties = ref([]);
 
   onMounted(async () => {
     // when the component first mounts it get the memes from the blockchain
     try {
-
       const bountyClaims = await getBountyClaims(accountId);
 
       userBounties.value = await Promise.all(
-        bountyClaims.map(async (bounty) => {
+        bountyClaims.map(async bounty => {
           const info = await getBounty(bounty.bounty_id);
-        //  console.log('bounty info' , bounty,info)
+          //  console.log('bounty info' , bounty,info)
           return {
             bounty,
             info,
             amount: NEAR.parse(`${info.amount} yN`).toHuman(),
-            deadline: format(
-              new Date(
-                fromUnixTime(
-                  parseInt(
-                    bounty.start_time / 1000000000 +
-                      bounty.deadline / 1000000000
-                  )
-                )
-              ),
-              "HH:mm, dd MMMM yyyy"
-            )
+            deadline: format(new Date(fromUnixTime(parseInt(bounty.start_time / 1000000000 + bounty.deadline / 1000000000))), 'HH:mm, dd MMMM yyyy')
           };
-          
         })
       );
     } catch (error) {
       console.error(error);
-      alert(error)
+      alert(error);
     }
   });
 
@@ -60,13 +35,13 @@ export const useBounties = (accountId) => {
     BountyDone(id, account_id, description);
   };
 
-  const handleBountyGiveUp = (id) => {
+  const handleBountyGiveUp = id => {
     BountyGiveUp(id);
   };
 
   return {
     userBounties,
     handleBountyDone,
-    handleBountyGiveUp,
+    handleBountyGiveUp
   };
 };
