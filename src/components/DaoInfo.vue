0d673eac6d13223d6e47f48b1ab076e9620b74a0
hiba <hiba.mashfij@gmail.com>
2021-10-11 17:23:56 +0300

    Updated heroSection and modals in NimDao

---
+++ src/components/DaoInfo.vue
@@ -0,0 +1,43 @@
+<template>
+ <div class="relative max-w-6xl m-auto mt-20">
+      <div class="text-center">
+        <h2 class="text-3xl tracking-tight font-extrabold sm:text-4xl text-near-green">
+          How It Works
+        </h2>
+         <p class="mt-3 text-xl sm:mt-4 text-near-gray-dark">
+          NimDAO funds people to create videos about NEAR.
+        </p>
+      </div>
+        <ul role="list" class="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4 mt-10">
+      <li v-for="item in infos" :key="item.title" class="col-span-1">
+        
+   
+          <div class="flex flex-col items-center rounded-lg justify-center px-4 py-2 ">
+            <h1 class="text-gray-900 font-medium hover:text-near-green text-md">{{ item.title }}</h1>
+            <p class="text-gray-500 text-sm mt-2 text-center">{{ item.description }} Members</p>
+          </div>
+         
+  
+      </li>
+    </ul>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+
+  setup() {
+     const infos = [
+  { title: 'Select a bounty', description: 'Browse the list of bounties and select the most suitable bounty for you', image: '#' },
+  { title: 'Claim the bounty', description: 'Check the deadline of each bounty and claim it', image: '#'  },
+  { title: 'Do your magic', description: 'Create your work and make sure to submit in the My bounties page before the deadline', image: '#'  },
+  { title: 'Get paid', description: 'Wait until your work is approved and check the result bounty done proposal to earn your reward', image: '#'  },
+]
+    return {
+      infos,
+    }
+  },
+};
+</script>
--- src/services/near.js
+++ src/services/near.js
@@ -1,82 +1,78 @@
-import { keyStores, Near, WalletConnection, utils } from "near-api-js";
-import BN from "bn.js";
-export const CONTRACT_ID = "hundred.testnet";
+import { keyStores, Near, WalletConnection, utils } from 'near-api-js';
+import BN from 'bn.js';
+export const CONTRACT_ID = 'hundred.testnet';
 
-const gas = new BN("70000000000000");
+const gas = new BN('70000000000000');
 // use new NEAR here to avoid needing async/await
 export const near = new Near({
-  networkId: "testnet",
+  networkId: 'testnet',
   keyStore: new keyStores.BrowserLocalStorageKeyStore(),
-  nodeUrl: "https://rpc.testnet.near.org",
-  walletUrl: "https://wallet.testnet.near.org",
+  nodeUrl: 'https://rpc.testnet.near.org',
+  walletUrl: 'https://wallet.testnet.near.org'
 });
 
 // can now create a new WalletConnection with the created Near object
-export const wallet = new WalletConnection(near, "hundred");
-
+export const wallet = new WalletConnection(near, 'hundred');
 
 export const getLastProposalId = () => {
-  return wallet.account().viewFunction(CONTRACT_ID, "get_last_proposal_id", {});
+  return wallet.account().viewFunction(CONTRACT_ID, 'get_last_proposal_id', {});
 };
 
 export const getProposals = async () => {
   const lastId = await getLastProposalId();
-  return wallet.account().viewFunction(CONTRACT_ID, "get_proposals", {
+  return wallet.account().viewFunction(CONTRACT_ID, 'get_proposals', {
     from_index: 0,
-    limit: lastId,
+    limit: lastId
   });
 };
 
 export const getLastBountyId = () => {
-  return wallet.account().viewFunction(CONTRACT_ID, "get_last_bounty_id", {});
+  return wallet.account().viewFunction(CONTRACT_ID, 'get_last_bounty_id', {});
 };
 
 export const getBounties = async () => {
   const lastId = await getLastBountyId();
-  return wallet.account().viewFunction(CONTRACT_ID, "get_bounties", {
+  return wallet.account().viewFunction(CONTRACT_ID, 'get_bounties', {
     from_index: 0,
-    limit: lastId,
+    limit: lastId
   });
 };
 
-export const getBountyNumberOfClaims = (id) => {
-  return wallet
-    .account()
-    .viewFunction(CONTRACT_ID, "get_bounty_number_of_claims", {
-      id: id,
-    });
+export const getBountyNumberOfClaims = id => {
+  return wallet.account().viewFunction(CONTRACT_ID, 'get_bounty_number_of_claims', {
+    id: id
+  });
 };
-
 
 export const BountyClaim = (id, deadline) => {
   return wallet.account().functionCall({
     contractId: CONTRACT_ID,
-    methodName: "bounty_claim",
+    methodName: 'bounty_claim',
     gas,
     args: {
       id: id,
-      deadline: deadline,
+      deadline: deadline
     },
-    attachedDeposit: utils.format.parseNearAmount("1"),
+    attachedDeposit: utils.format.parseNearAmount('1')
   });
 };
 
-export const getBountyClaims = (accountId) => {
-  return wallet.account().viewFunction(CONTRACT_ID, "get_bounty_claims", {
-    account_id: accountId,
+export const getBountyClaims = accountId => {
+  return wallet.account().viewFunction(CONTRACT_ID, 'get_bounty_claims', {
+    account_id: accountId
   });
 };
 
-export const getBounty = (id) => {
-  return wallet.account().viewFunction(CONTRACT_ID, "get_bounty", {
-    id: id,
+export const getBounty = id => {
+  return wallet.account().viewFunction(CONTRACT_ID, 'get_bounty', {
+    id: id
   });
 };
 
 export const BountyDone = (id, account_id, description) => {
   return wallet.account().functionCall({
     contractId: CONTRACT_ID,
-    methodName: "add_proposal",
+    methodName: 'add_proposal',
     gas,
     args: {
       proposal: {
@@ -84,22 +80,22 @@ export const BountyDone = (id, account_id, description) => {
         kind: {
           BountyDone: {
             bounty_id: id,
-            receiver_id: account_id,
-          },
-        },
-      },
+            receiver_id: account_id
+          }
+        }
+      }
     },
-    attachedDeposit: utils.format.parseNearAmount("3"),
+    attachedDeposit: utils.format.parseNearAmount('3')
   });
 };
 
-export const BountyGiveUp = (id) => {
+export const BountyGiveUp = id => {
   return wallet.account().functionCall({
     contractId: CONTRACT_ID,
-    methodName: "bounty_giveup",
+    methodName: 'bounty_giveup',
     gas,
     args: {
-      id: id,
+      id: id
     }
   });
 };
