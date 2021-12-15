8e8e5e7df5d89c9e2b8c56e4f1e668fb1312e0be
hiba <hiba.mashfij@gmail.com>
2021-12-15 14:01:34 +0300

    fixed navigation responsivness

--- src/components/layout/NavigationComponent.vue
+++ src/components/layout/NavigationComponent.vue
@@ -82,8 +82,63 @@
     </div>
 
     <DisclosurePanel class="lg:hidden">
-      <div class="pt-2 pb-3 space-y-1">
-        <router-link v-for="item in nav" :key="item.name" :to="item.href" :class="[item.current ? 'bg-gray-100 text-gray-900' : 'text-gray-900 hover:bg-gray-50 hover:text-gray-900', 'block rounded-md py-2 px-3 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.label }}</router-link>
+      <div class="pt-2 space-y-1 ml-2">
+        <!-- <router-link v-for="item in nav" :key="item.name" :to="item.href" :class="[item.current ? 'bg-gray-100 text-gray-900' : 'text-gray-900 hover:bg-gray-50 hover:text-gray-900', 'block rounded-md py-2 px-3 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.label }}</router-link> -->
+        <Popover class="relative" v-slot="{ open }">
+          <PopoverButton :class="[open ? 'text-gray-900' : 'text-gray-600 hover:bg-near-gray hover:text-black', nav1[0].current | nav1[1].current ? 'bg-near-gray ' : 'text-gray-600 hover:bg-near-gray hover:text-black', 'rounded-md mt-4 px-3 inline-flex items-center font-medium h-10']">
+            <router-link :to="nav[0].href">
+              {{ nav[0].label }}
+            </router-link>
+            <ChevronDownIcon :class="[open ? 'text-gray-600' : 'text-gray-400', 'ml-2 h-5 w-5 group-hover:text-gray-500']" aria-hidden="true" />
+          </PopoverButton>
+
+          <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
+            <PopoverPanel class="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0">
+              <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                  <a v-for="item in nav1" :key="item.name" :href="item.href" :class="[item.current ? 'bg-near-gray ' : 'text-gray-600 hover:bg-near-gray hover:text-black', '-m-3 p-3 block rounded-md hover:bg-gray-50']">
+                    <p class="text-base font-medium text-gray-900">
+                      {{ item.label }}
+                    </p>
+                    <p class="mt-1 text-sm text-gray-500">
+                      {{ item.description }}
+                    </p>
+                  </a>
+                </div>
+              </div>
+            </PopoverPanel>
+          </transition>
+        </Popover>
+
+        <Popover class="relative" v-slot="{ open }">
+          <PopoverButton :class="[open ? 'text-gray-900' : 'text-gray-600 hover:bg-near-gray hover:text-black', nav2[0].current | nav2[1].current ? 'bg-near-gray ' : 'text-gray-600 hover:bg-near-gray hover:text-black', 'rounded-md my-4 px-3 inline-flex items-center font-medium h-10']">
+            <router-link :to="nav[1].href">
+              {{ nav[1].label }}
+            </router-link>
+            <ChevronDownIcon :class="[open ? 'text-gray-600' : 'text-gray-400', 'ml-2 h-5 w-5 group-hover:text-gray-500']" aria-hidden="true" />
+          </PopoverButton>
+
+          <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
+            <PopoverPanel class="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0">
+              <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                  <a v-for="item in nav2" :key="item.name" :href="item.href" :class="[item.current ? 'bg-near-gray ' : 'text-gray-600 hover:bg-near-gray hover:text-black', '-m-3 p-3 block rounded-md hover:bg-gray-50']">
+                    <p class="text-base font-medium text-gray-900">
+                      {{ item.label }}
+                    </p>
+                    <p class="mt-1 text-sm text-gray-500">
+                      {{ item.description }}
+                    </p>
+                  </a>
+                </div>
+              </div>
+            </PopoverPanel>
+          </transition>
+        </Popover>
+
+        <router-link :to="nav[2].href" :class="[nav[2].current ? 'bg-near-gray ' : 'text-gray-600 hover:bg-near-gray hover:text-black', 'rounded-md my-4 px-3 inline-flex items-center font-medium h-10']">
+          {{ nav[2].label }}
+        </router-link>
       </div>
       <div class="pt-2 pb-3 px-2">
         <NearLogin />
